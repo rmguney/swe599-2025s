5b4b3fd865f1463669b7f16f4a2365eaae135837
rmguney <rmguney@gmail.com>
2025-03-07 00:41:57 +0300

    fixed rt bug, compiled with better resolution

--- WebGL/public/Build/public.loader.js
+++ WebGL/public/Build/public.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 91434084,
+    wasmFileSize: 91439036,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
